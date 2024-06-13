d971c42d419a3b331825076ee501760e02b0828b
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-06-13 16:47:33 -0300

    Init React App

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles.js";
+import { Button } from "../../components/Button";
+
+export function Details() {
+    return (
+      <Container>
+        <h1>Hello World!</h1>
+        <span>Rodrigo Gonçalves</span>
+        <Button />
+      </Container>
+    )
+}
