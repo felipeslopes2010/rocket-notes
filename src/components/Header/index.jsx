e694ab1b6564332531b323f43cf7630af72e7623
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-06-17 10:23:21 -0300

    Create Tag Component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,23 @@
-import { Container } from "./styles";
+import { RiShutDownLine } from "react-icons/ri";
+
+import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
     return (
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/orodrigogo.png"
+                    alt="Foto do usuário"
+                />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Rodrigo Gonçalves</strong>
+                </div>
+            </Profile>
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
         </Container>
     )
 }
