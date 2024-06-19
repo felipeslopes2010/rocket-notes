17ffd1724075819f69724cdd7f6f749c2cacd7f0
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-06-19 18:27:21 -0300

    Add Menu Container

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -4,5 +4,5 @@ export const Container = styled.button`
         background: none;
         border: none;
         font-size: 16px;
-        color: ${({theme}) => theme.COLORS.ORANGE};
+        color: ${({theme, isActive}) => isActive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100};
 `;
