5d7bb67fe58bdf74bc876a1e1501918fa041c221
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-06-24 17:48:23 -0300

    Create SingIn Page

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: stretch;
+`;
+
+export const Form = styled.form`
+    padding: 0 136px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    > h1 {
+        font-size: 48px;
+        color: ${({theme}) => theme.COLORS.ORANGE};
+    }
+    
+    p {
+        font-size: 14px;
+        margin-bottom: 48px;
+        color: ${({theme}) => theme.COLORS.GRAY_100};
+    }
+
+    > h2 {
+        font-size: 24px;
+        margin-bottom: 48px;
+        color: ${({theme}) => theme.COLORS.WHITE};
+    }
+
+    > a {
+        margin-top: 124px;
+        font-size: 16px;
+        color: ${({theme}) => theme.COLORS.ORANGE};
+    }
+`;
