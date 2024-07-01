178ca92c12afb8cb0502ab08d391618ca370e1a0
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-07-01 14:41:25 -0300

    Customize Profile Page

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -23,4 +23,50 @@ export const Container = styled.div`
 export const Form = styled.form`
     max-width: 340px;
     margin: 30px auto 0;
+
+    > div:nth-child(4) {
+        margin-top: 24px;
+    }
+`;
+
+export const Avatar = styled.form`
+    position: relative;
+    margin: -124px auto 32px;
+
+    width: 186px;
+    height: 186px;
+
+    > img {
+        width: 186px;
+        height: 186px;
+        border-radius: 50%;
+    }
+
+    > label {
+        height: 48px;
+        width: 48px;
+
+        background-color: ${({ theme }) => theme.COLORS.ORANGE};
+        border-radius: 50%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        bottom: 7px;
+        right: 7px;
+
+        cursor: pointer;
+
+        input {
+            display: none;
+        }
+
+        svg {
+            width: 20px;
+            height: 20px;
+            color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+        }
+    }
 `;
