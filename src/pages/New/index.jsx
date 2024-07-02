87e65dfb342a73c96bc24905590e80cfc972c701
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-07-02 10:43:48 -0300

    Create TextArea Component

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,26 @@
-import { Header } from "../../components/Header"
-import { Container } from "./styles";
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { TextArea } from '../../components/TextArea'
+
+import { Container, Form } from './styles'
 
 export function New() {
-    return (
-        <Container>
-            <Header />
-        </Container>
-    );
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+
+          <TextArea placeholder="Observações" />
+        </Form>
+      </main>
+    </Container>
+  )
 }
