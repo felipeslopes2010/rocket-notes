d973114f76343a80596baed016dbdbd0deb71f04
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-07-04 20:42:20 -0300

    Add NoteItem Container

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { TextArea } from '../../components/TextArea'
+import { Section } from '../../components/Section'
+import { NoteItem } from '../../components/NoteItem'
 
 import { Container, Form } from './styles'
 
@@ -19,6 +21,14 @@ export function New() {
           <Input placeholder="Título" />
 
           <TextArea placeholder="Observações" />
+
+          <Section title="Links úteis">
+
+            <NoteItem value={"https://www.rocketseat.com.br/"} />
+
+            <NoteItem isNew />
+
+          </Section>
         </Form>
       </main>
     </Container>
