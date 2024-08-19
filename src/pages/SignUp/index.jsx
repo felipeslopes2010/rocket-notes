9191720e82f1a76076adaccce12e1262e652a9cd
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-08-19 14:38:19 -0300

    Import: Axios

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Link } from "react-router-dom";
+
+import { api } from "../../s"
 
 import { Container, Form, Background } from './styles'
 
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 
-
 export function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    function handleSignUp() {
+        if(!name || !email || !password) {
+            return alert("Preencha todos os campos!");
+        }
+    }
+
     return (
         <Container>
             <Background />
@@ -22,21 +34,24 @@ export function SignUp() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Cadastrar" />
+                <Button title="Cadastrar" onClick={handleSignUp} />
 
                 <Link to="/">
                     Voltar para o login
