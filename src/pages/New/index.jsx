bb8cba20dafcd0d944292f4f65e778e383a17eee
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-08-27 15:35:19 -0300

    Add: Remove link function

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -19,6 +19,10 @@ export function New() {
     setNewLink("");
   }
 
+  function handleRemoveLink(deleted) {
+    setLinks(prevState => prevState.filter(link => link !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -40,11 +44,11 @@ export function New() {
                 <NoteItem
                   key={String(index)}
                   value={link}
-                  onClick={() => { }}
+                  onClick={() => handleRemoveLink(link)}
                 />
               ))
             }
-            
+
             <NoteItem
               isNew
               placeholder="Novo link"
