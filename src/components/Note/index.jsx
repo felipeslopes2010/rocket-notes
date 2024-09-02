80bc98d1db159769b225da229b54b3bbd967516d
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-09-02 15:17:01 -0300

    Functionality: Open details on notes after click

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -3,18 +3,18 @@ import { Container } from './styles';
 import { Tag } from '../Tag';
 
 export function Note({ data, ...rest }) {
-    return (
-        <Container>
-            <h1>{data.title}</h1>
+  return (
+    <Container {...rest}>
+      <h1>{data.title}</h1>
 
-            {
-                data.tags &&
-                <footer>
-                    {
-                        data.tags.map(tag => <Tag key={tag.id} title={tag.name} />)
-                    }
-                </footer>
-            }
-        </Container>
-    );
+      {
+        data.tags &&
+        <footer>
+          {
+            data.tags.map(tag => <Tag key={tag.id} title={tag.name} />)
+          }
+        </footer>
+      }
+    </Container>
+  );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 
 import { ButtonText } from '../../components/ButtonText';
@@ -17,6 +18,8 @@ export function Home() {
     const [tagsSelected, setTagsSelected] = useState([]);
     const [notes, setNotes] = useState([]);
 
+    const navigate = useNavigate();
+
     function handleTagSelected(tagName) {
         if(tagName === "all") {
             return setTagsSelected([]);
@@ -30,6 +33,12 @@ export function Home() {
         } else {
             setTagsSelected(prevState => [...prevState, tagName]);
         }
+    }
+
+    function handleDetails(id) {
+        console.log('id');
+        
+        navigate(`/details/${id}`);
     }
     
     useEffect(() => {
@@ -93,6 +102,7 @@ export function Home() {
                             <Note
                                 key={String(note.id)} 
                                 data={note}
+                                onClick={() => handleDetails(note.id)}
                             />
                         ))
                     }
